5fa34375a1396c1918337d4be38625677b71f69f
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2024-07-10 18:30:42 -0400

    fix: timescaledb run.sh

--- timescaledb-compressed-external/run.sh
+++ timescaledb-compressed-external/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 TRIES=3
+QUERY_NUM=1
 
 cat queries.sql | while read query; do
-    sync
-    echo 3 | sudo tee /proc/sys/vm/drop_caches
+    # sync
+    # echo 3 | sudo tee /proc/sys/vm/drop_caches
 
-    echo "$query";
+    echo -n "["
     for i in $(seq 1 $TRIES); do
-        psql -U postgres -h "$HOST" -p 9001 -d "$DATABASE" -t -c '\timing' -c "$query" | grep 'Time'
+        RES=$(psql -U postgres -h "$HOST" -p 9001 -d "$DATABASE" -t -c '\timing' -c "$query" | grep 'Time' 2>&1 ||:)
+        [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
+        [[ "$i" != $TRIES ]] && echo -n ", "
     done;
+    echo "],"
+
+    QUERY_NUM=$((QUERY_NUM + 1))
 done;
