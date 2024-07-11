4cafc314c70adad0e2a93897e1e6fbdef7440acc
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2024-07-11 19:17:43 -0400

    fix: store results

--- timescaledb-compressed-external/benchmark.sh
+++ timescaledb-compressed-external/benchmark.sh
@@ -36,7 +36,7 @@ psql -U postgres -h "$HOST" -p 9001 -d "$DATABASE" -t -c '\timing' -c "\\copy hi
 time psql -U postgres -h "$HOST" -p 9001 -d "$DATABASE" -c "SELECT compress_chunk(i, if_not_compressed => true) FROM show_chunks('hits') i"
 
 # Run the queries
-./run.sh 2>&1 | tee -a $RESULTDIR/$FILENAME.log
+./run.sh &> $RESULTDIR/$FILENAME.log
 
 cat $RESULTDIR/$FILENAME.log
 rm hits.tsv.gz
