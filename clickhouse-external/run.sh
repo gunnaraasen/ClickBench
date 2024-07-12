b94ee0e627606e142141ab9cd93c6b404b7dbd55
Sara Ghodsi <55705790+saraghds@users.noreply.github.com>
2024-07-12 10:07:34 -0400

    fix: clickhouse time

--- clickhouse-external/run.sh
+++ clickhouse-external/run.sh
@@ -9,7 +9,7 @@ cat queries.sql | while read query; do
     echo -n "{\"query\": \""$query"\","
     echo -n "\"runtimes\": ["
     for i in $(seq 1 $TRIES); do
-        RES=$(clickhouse-client --host "${FQDN:=localhost}" --user "$USER" --password "${PASSWORD:=}" --database "$DATABASE" --time --format=Null --query="$query" --progress 0 2>&1 ||:)
+        RES=$(clickhouse-client --host "${FQDN:=localhost}" --user "$USER" --password "${PASSWORD:=}" --database "$DATABASE" --time --format=Null --query="$query" --progress 0 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' 2>&1 ||:)
         [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
         [[ "$i" != $TRIES ]] && echo -n ", "
     done
